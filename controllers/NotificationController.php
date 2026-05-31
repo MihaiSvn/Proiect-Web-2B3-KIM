8119915186a34e03449e6566d948aaf66bab72d7
MihaiSvn <mihaialexandrusavin@gmail.com>
2026-05-31 20:00:21 +0300

    fixes to notifications controller

--- controllers/NotificationController.php
+++ controllers/NotificationController.php
@@ -14,43 +14,56 @@ class NotificationController
     public function markAllAsRead()
     {
         if (!isset($_SESSION['user_id'])) {
-            header('Location: /kim/login');
+            header('Location: /kim/login?error=You have to be logged in to see this page');
             exit;
         }
 
         $userId = $_SESSION['user_id'];
         $this->notificationService->dismissNotifications($userId);
 
-        $this->redirectBack();
+        $this->redirectBack('success', 'All notifications have been marked as read');
     }
 
     public function markAsRead()
     {
         if (!isset($_SESSION['user_id'])) {
-            header('Location: /kim/login');
+            header('Location: /kim/login?error=You have to be logged in to see this page');
             exit;
+        }
+
+        if (!isset($_POST['notification_id'])) {
+            $this->redirectBack('error', 'Missing notification ID.');
+            return;
         }
 
         if (isset($_POST['notification_id'])) {
             $notificationId = (int)$_POST['notification_id'];
-            $this->notificationService->dismissNotificationById($notificationId);
+            try {
+                $this->notificationService->dismissNotificationById($notificationId);
+
+                $this->redirectBack('success', 'Notification has been marked as read.');
+
+            } catch (\InvalidArgumentException $e) {
+                $this->redirectBack('error', $e->getMessage());
+            }
         }
 
-        $this->redirectBack();
+
     }
 
-    /**
-     * Helper privat: Redirecționează userul înapoi de unde a venit
-     */
-    private function redirectBack()
+
+    private function redirectBack($statusType, $message)
     {
-        // Dacă știm de pe ce pagină a venit cererea, îl trimitem fix acolo
-        if (isset($_SERVER['HTTP_REFERER'])) {
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-        } else {
-            // Fallback în caz că browserul nu trimite HTTP_REFERER
-            header('Location: /kim/dashboard');
-        }
+
+        $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/kim/dashboard';
+
+        //daca in link avem deja un parametru sau nu
+        $separator = (strpos($referer, '?') !== false) ? '&' : '?';
+
+        // ?success=Mesaj+aici)
+        $queryString = ($statusType !== '') ? $separator . $statusType . '=' . urlencode($message) : '';
+
+        header('Location: ' . $referer . $queryString);
         exit;
     }
 }
