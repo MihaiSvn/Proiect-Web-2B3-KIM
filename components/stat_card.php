af64c513919d6b8f87262d9fb99399be06bd89c7
MihaiSvn <mihaialexandrusavin@gmail.com>
2026-06-02 00:54:12 +0300

    finsihed trainer dashboard

--- components/stat_card.php
+++ components/stat_card.php
@@ -1,15 +1,44 @@
+<?php
+/**
+ * @var numeric $trend // it can be null, if it exists it's a number
+ * @var string $cardIcon // icon for the card
+ * @var string $iconColor // color for icon
+ * @var string $iconBgColor //color pt background icon
+ * @var string $statTitle //title for card
+ * @var string $statText // actual stat
+ */
+?>
+
+
+<?php
+
+//trend sa fie un numar
+    if(isset($trend)){
+        if($trend<0){
+            $trendClass = 'trend-down';
+            $trendText = $trend  . "%";
+        } else {
+            $trendClass = 'trend-up';
+            $trendText = "+" . $trend  . "%";
+        }
+    }
+
+?>
 <div class="stat-card">
     <div class="stat-card__header">
-        <div class="stat-card__icon">
-            <i class="fa-solid fa-user-group"></i>
+        <div class="stat-card__icon" style="color: <?= $iconColor ?>; background-color: <?= $iconBgColor ?>">
+            <i class="<?= $cardIcon ?>"></i>
         </div>
-        <div class="stat-card__trend trend-up">
-            +12%
+        <?php if(isset($trend)): ?>
+        <div class="stat-card__trend <?= $trendClass ?>">
+            <?= $trendText ?>
         </div>
+        <?php endif;?>
     </div>
 
     <div class="stat-card__body">
-        <h3 class="stat-card__value">1,254</h3>
-        <p class="stat-card__label">Active Members</p>
+        <p class="stat-card__label"><?= $statTitle?></p>
+        <h3 class="stat-card__value"> <?= $statText ?></h3>
+
     </div>
 </div>
