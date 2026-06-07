6bfed8621108cd39a3cd2880a98052bcf8afc3b3
MihaiSvn <mihaialexandrusavin@gmail.com>
2026-06-07 13:32:42 +0300

    start

---
+++ middleware/ApiAuthMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace middleware;
+
+class ApiAuthMiddleware
+{
+    public static function checkAccess(){
+
+
+        header('content-type: application/json');
+        $requestedUserId = isset($_GET['user_id']) ? $_GET['user_id'] : null;
+        $loggedInUserId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+        $loggedInUserRole = isset($_SESSION['user_role']) ? $_SESSION['user_role'] : null;
+
+        if(!$loggedInUserId){
+            http_response_code(401);
+            echo json_encode(['error' => 'You are not authorized to access this page.']);
+            exit;
+        }
+
+        if($requestedUserId !== null && $loggedInUserId !== $requestedUserId && $loggedInUserRole !== "admin"){
+            http_response_code(401);
+            echo json_encode(['error' => 'You are not authorized to access this page.']);
+            exit;
+        }
+    }
+}
